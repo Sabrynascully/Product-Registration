ff1ec01c967675503c16c3239785a70acd049586
Sabryna Marques <scullysabrina8@gmail.com>
2022-08-12 21:10:26 -0300

    Componente Obter Produto

--- frontend/cadastro-produtos/src/app/app.module.ts
+++ frontend/cadastro-produtos/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 
@@ -38,6 +39,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     ProductCrudComponent,
     RedDirective,
     ProductCreateComponent,
+    ProductReadComponent,
     
 
   ],
