b3d9a487daeeda50bf758dbfecc745b9c2a35183
Sabryna Marques <scullysabrina8@gmail.com>
2022-08-23 19:46:28 -0300

    Componente Atualizar Produto

--- frontend/cadastro-produtos/src/app/app-routing.module.ts
+++ frontend/cadastro-produtos/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'products', component: ProductCrudComponent},
 {path: 'home', component: HomeComponent},
-{path: 'products/create', component: ProductCreateComponent }
+{path: 'products/create', component: ProductCreateComponent },
+{path: 'products/update/id', component: ProductUpdateComponent }
 ];
 
 @NgModule({
